0712d7905680f4e5b91d09c5a59d59b13db79695
Yash Kumar Gola <151176007+crimsonyash9012@users.noreply.github.com>
2025-07-20 19:30:13 +0530

    Delete CryptoTracker-master directory

--- CryptoTracker-master/app/src/main/java/com/plcoding/cryptotracker/crypto/domain/CoinPrice.kt
+++
@@ -1,9 +0,0 @@
-package com.plcoding.cryptotracker.crypto.domain
-
-import java.time.ZonedDateTime
-
-data class CoinPrice(
-    val priceUsd: Double,
-    // we can also use Long over here, but difficult to convert it
-    val dateTime: ZonedDateTime
-)
--- CryptoTracker-master/app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.plcoding.cryptotracker.crypto.presentation.coin_list
-
-import com.plcoding.cryptotracker.core.domain.util.NetworkError
-
-sealed interface CoinListEvent {
-    // we want to pass the error message to our ui whenever error occurs
-    data class Error(val error: NetworkError): CoinListEvent
-}
